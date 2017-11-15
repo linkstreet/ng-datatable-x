2795501ee54a9b7f99db759743a0bd21583a9d0c
Akhilesh Bamhore <akhilesh@linkstreet.in>
2017-11-15 14:51:49 +0530

    TASK: Material Updated

--- src/datatable.module.ts
+++ src/datatable.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MdInputModule, MdSelectModule} from '@angular/material';
+import {MatInputModule, MatSelectModule} from '@angular/material';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {DataTableXComponent} from './components';
 @NgModule({
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, MdInputModule, MdSelectModule, NgbModule.forRoot()],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule, MatSelectModule, NgbModule.forRoot()],
     declarations: [DataTableXComponent],
     exports: [DataTableXComponent]
 })
