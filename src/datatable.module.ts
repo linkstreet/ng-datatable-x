234bffbb667185d3c6a3f91270830b6f5899f1a9
Akhilesh Bamhore <akhilesh@linkstreet.in>
2017-11-27 15:32:16 +0530

    TASK: ng-datatable-x with angular 5

--- src/datatable.module.ts
+++ src/datatable.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule, MatSelectModule} from '@angular/material';
@@ -7,6 +7,7 @@ import {DataTableXComponent} from './components';
 @NgModule({
     imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule, MatSelectModule, NgbModule.forRoot()],
     declarations: [DataTableXComponent],
-    exports: [DataTableXComponent]
+    exports: [DataTableXComponent],
+    schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NgDataTableX {}
